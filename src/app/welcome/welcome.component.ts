e3650264cbf61283f583977acbd2b50514f8a33c
Dmytro Zeleniuk <dimaszelenyuk@gmail.com>
2024-06-22 00:29:00 -0400

    add play btn

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component} from "@angular/core";
+import {Component} from "@angular/core";
 import { environment } from '../../environments/environment';
 
 @Component({
@@ -7,23 +7,29 @@ import { environment } from '../../environments/environment';
   styleUrls: ["./welcome.component.css", "./welcome-component.responsivity.css"],
 })
 
-export class WelcomeComponent implements AfterViewInit{
+export class WelcomeComponent{
 
-  public illustration:string = './assets/template/welcome/dmytro-illustration-crop.gif';
+  // public illustration:string = './assets/template/welcome/dmytro-illustration-crop.gif';
+  public illustration:string = './assets/template/welcome/dmytro-illustration-active.png';
   public sound:string = 'assets/sounds/illustration.mp3';
 
   constructor() {}
 
-  ngAfterViewInit() {
-    const imgElement = document.getElementById('illustration');
+  public playIllustration(): void {
+    const btnElement = document.getElementById('play-illustration');
+    const illustrationElement = document.getElementById('illustration');
+    this.illustration = './assets/template/welcome/dmytro-illustration-crop.gif';
     const audio = new Audio(this.sound);
     audio.play();
+    btnElement.classList.add('hide-play-btn');
+
     audio.addEventListener('ended', () => {
-      imgElement.classList.add('image-fade-out');
+      illustrationElement.classList.add('image-fade-out');
       setTimeout(() => {
         this.illustration = './assets/template/welcome/dmytro-illustration-active.png';
-        imgElement.classList.remove('image-fade-out');
-      }, 2000);
+        illustrationElement.classList.remove('image-fade-out');
+        btnElement.classList.remove('hide-play-btn');
+      }, 100);
 
     });
   }
